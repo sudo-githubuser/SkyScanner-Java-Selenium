d83fd9fdf38c72ce7ef407a612d5482bf0bd4f5b
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2025-07-13 02:24:18 +0530

    Utility class, added code for select class

--- src/main/java/utility/Utils.java
+++ src/main/java/utility/Utils.java
@@ -3,6 +3,7 @@ package utility;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Utils {
 
@@ -17,6 +18,36 @@ public class Utils {
                 "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});",
                 element
         );
+    }
+
+    /**
+     * Selects an option from the dropdown
+     *
+     * @param visibleText/value/index of the select class
+     * @param element the WebElement of the select class
+     */
+
+    public static void selectByVisibleText(WebElement element, String visibleText) {
+        validateSelectElement(element);
+        new Select(element).selectByVisibleText(visibleText);
+    }
+
+    public static void selectByValue(WebElement element, String value) {
+        validateSelectElement(element);
+        new Select(element).selectByValue(value);
+    }
+
+    public static void selectByIndex(WebElement element, int index) {
+        validateSelectElement(element);
+        var dropdown = new Select(element);
+        var selectedText = dropdown.getOptions().get(index).getText();
+        dropdown.selectByIndex(index);
+    }
+
+    private static void validateSelectElement(WebElement element) {
+        if (!"select".equalsIgnoreCase(element.getTagName())) {
+            throw new IllegalArgumentException("Element is not a <select> tag");
+        }
     }
 
 }
