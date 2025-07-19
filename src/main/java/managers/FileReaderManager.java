f038476369e3afeb19f6a7ff8662696e0bde8d4b
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2025-07-20 00:25:49 +0530

    WebDriverManager, FileReaderManager

--- src/main/java/managers/FileReaderManager.java
+++ src/main/java/managers/FileReaderManager.java
@@ -1,4 +1,25 @@
 package managers;
 
+import dataProvider.ConfigFileReader;
+
 public class FileReaderManager {
+
+    private FileReaderManager(){}
+
+    private static class Holder{
+        private static final FileReaderManager INSTANCE = new FileReaderManager();
+
+        private static ConfigFileReader configFileReader;
+    }
+
+    public static FileReaderManager getInstance(){
+        return Holder.INSTANCE;
+    }
+
+    public static ConfigFileReader getConfigReader(){
+        if(Holder.configFileReader == null){
+            Holder.configFileReader = new ConfigFileReader();
+        }
+        return Holder.configFileReader;
+    }
 }
--- src/main/java/managers/WebDriverManager.java
+++ src/main/java/managers/WebDriverManager.java
@@ -2,10 +2,50 @@ package managers;
 
 import enums.DriverType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import java.time.Duration;
 
 public class WebDriverManager {
 
     private WebDriver driver;
-    private static DriverType driverType;
+    private final DriverType driverType;
 
+    public WebDriverManager() {
+        this.driverType = FileReaderManager.getConfigReader().getBrowser();
+    }
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            driver = createDriver();
+        }
+        return driver;
+    }
+
+    private WebDriver createDriver() {
+        switch (driverType) {
+            case FIREFOX -> driver = new FirefoxDriver();
+            case CHROME -> driver = new ChromeDriver();
+            case INTERNETEXPLORER -> driver = new InternetExplorerDriver();
+            default -> throw new IllegalArgumentException("Unsupported browser: " + driverType);
+        }
+
+        driver.manage().timeouts().implicitlyWait(FileReaderManager.getConfigReader().getImplicitWait());
+        return driver;
+    }
+
+    public void closeDriver() {
+        if (driver != null) {
+            driver.close();
+        }
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null; // Reset to allow re-creation if needed
+        }
+    }
 }
+
